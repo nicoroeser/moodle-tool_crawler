abd1fea3766a16847616da616a78d9256e78102e
Nicolas Roeser <nicolas.roeser@uni-ulm.de>
2019-05-09 14:26:07 +0200

    [DEVEL, TESTING] Reduce sizes for a test to be successful

--- testserver/routes/index.js
+++ testserver/routes/index.js
@@ -43,13 +43,13 @@ router.get('/headgettest', function(req, res) {
 router.get('/reallylong', function(req, res) {
     res.set('Content-Type', '             text/plain     ');
     res.set('Location', '/reallylong1.5');
-    for (i = 0; i < 180; i++) {
+    for (i = 0; i < 1; i++) {
         res.set('X-Foobar-Location-Blah-Filler-' + i, 'Useless header content (' + i + ') after sending Location header field');
     }
     res.status(302, 'Moved Temporarily');
     res.write('<!DOCTYPE html>\n<html><head><title>reallylong document has moved</title></head>\n');
     res.write('<body><p><a href="/reallylong1.5">really long document</a> has moved.</p>\n');
-    for (i = 0; i < 30000; i++) {
+    for (i = 0; i < 1; i++) {
         res.write('<p>This HTML document has moved.</p>\n');
         res.write('<p>Really! It has moved.</p>\n');
         res.write('<p>Only temporarily. But it has moved!</p>\n');
@@ -60,7 +60,7 @@ router.get('/reallylong', function(req, res) {
 
 router.get('/reallylong1.5', function(req, res) {
     res.set('Content-Type', 'text/html; charset=UTF-8');
-    for (i = 0; i < 100000; i++) {
+    for (i = 0; i < 3; i++) {
         res.set('X-Intermediate-Redirect-Filler-' + ('000000' + i).slice(-7),
                 'Very long header content which is really not necessary. xy');
                     // should produce a header line which is 100 octets in length (including CRLF)
@@ -69,7 +69,7 @@ router.get('/reallylong1.5', function(req, res) {
     res.status(302, 'Moved Temporarily AGAIN');
     res.write('<!DOCTYPE html>\n<html><head><title>reallylong document has moved (2nd)</title></head>\n');
     res.write('<body><p><a href="/reallylong2">really long document</a> has moved.</p>\n');
-    for (i = 0; i < 30000; i++) {
+    for (i = 0; i < 3; i++) {
         res.write('<p>This fine HTML document has moved.</p>\n');
         res.write('<p>Just temporarily.</p>\n');
     }
@@ -80,13 +80,13 @@ router.get('/reallylong1.5', function(req, res) {
 router.get('/reallylong2', function(req, res) {
     //res.set('Content-Length', '22');
     res.set('Content-Type', 'text/html');
-    for (i = 0; i < 200; i++) {
+    for (i = 0; i < 2; i++) {
         res.set('X-Foobar-Filler-' + i, 'Useless header content (' + i + ')');
     }
     res.status(200, 'OK');
     doc = '';
     doc += '<!DOCTYPE html>\n<html><head><title>This is a really long document.</title></head><body><p>foo</p>\n';
-    for (i = 0; i < 999; i++) {  // woah, this will need lots of memory (> 180 MB)
+    for (i = 0; i < 1; i++) {
         doc += '<p>This HTML document may actually be the longest one on earth. Who knows?</p>\n';
         doc += '<p>Here comes even more content. Blah, blah. You should stop downloading. Go outside, enjoy the sun!</p>\n';
     }
